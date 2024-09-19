e2c31750ab9753abaf53625fd63c8f2d4a1cef52
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2024-09-18 17:54:19 -0700

    Update polystring.js
    starting on extension

--- polystring.js
+++ polystring.js
@@ -1,1 +1,24 @@
-alert("test");
+// If "wild" is -1, unlimited length, >0: limited length
+var PolyString = {
+    "match": function(str,exp) {
+        var reqs = []; // format: {req,bool,mult,wild}
+        var n = 0;
+        var newprops = {"bool": true};
+        while(n <= exp.length) {
+            if(str[n] == "*") {
+                if(reqs[reqs.length - 1].wild != 0) {
+                    reqs[reqs.length - 1].wild = -1;
+                } else {
+                    reqs.push({"bool":newprops.bool,"wild":-1});
+                }
+            } else if(str[n] == "%") {
+                if(reqs[reqs.length - 1].wild > 0) {
+                    reqs[reqs.length - 1].wild += 1;
+                } else {
+                    reqs.push({"bool":newprops.bool,"wild":1});
+                }
+            }
+        }
+    },
+    "version":"1.0"
+}
