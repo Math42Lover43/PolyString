7389113b35420b447ad8d17f08e00e035097543e
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2024-09-19 16:39:45 -0700

    Update polystring.js
    caught TypeError

--- polystring.js
+++ polystring.js
@@ -6,13 +6,13 @@ var PolyString = {
         var newprops = {"bool": true};
         while(n < exp.length) {
             if(exp[n] == "*") {
-                if(reqs[reqs.length - 1].wild != 0 && typeof reqs[reqs.length - 1].wild == "number") {
+                if(reqs[reqs.length - 1] && reqs[reqs.length - 1].wild != 0 && typeof reqs[reqs.length - 1].wild == "number") {
                     reqs[reqs.length - 1].wild = -1;
                 } else {
                     reqs.push({"bool":newprops.bool,"wild":-1});
                 }
             } else if(exp[n] == "%") {
-                if(reqs[reqs.length - 1].wild > 0 && typeof reqs[reqs.length - 1].wild == "number") {
+                if(reqs[reqs.length - 1] && reqs[reqs.length - 1].wild > 0 && typeof reqs[reqs.length - 1].wild == "number") {
                     reqs[reqs.length - 1].wild += 1;
                 } else {
                     reqs.push({"bool":newprops.bool,"wild":1});
