5d589a4deeffd1e6ec2a77a301e566e1a74cde61
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2024-09-21 09:48:48 -0700

    Update polystring.js
    added "\"

--- polystring.js
+++ polystring.js
@@ -3,9 +3,16 @@ var PolyString = {
     "compile": function(exp) {
         var reqs = []; // format: {req,bool,mult,wild}
         var n = 0;
-        var newprops = {"bool": true};
+        var newprops = {"bool": true,"escaped":false};
         while(n < exp.length) {
-            if(exp[n] == "*") {
+            if(escaped) {
+                if(reqs[reqs.length - 1] && reqs[reqs.length - 1].wild == 0) {
+                    reqs[reqs.length - 1].req += exp[n];
+                } else {
+                    reqs.push({"reqs":exp[n],"bool":true,"wild":0});
+                }
+                newprops.escaped = false;
+            } else if(exp[n] == "*") {
                 if(reqs[reqs.length - 1] && reqs[reqs.length - 1].wild != 0 && typeof reqs[reqs.length - 1].wild == "number") {
                     reqs[reqs.length - 1].wild = -1;
                 } else {
@@ -19,6 +26,8 @@ var PolyString = {
                         reqs.push({"bool":newprops.bool,"wild":1});
                     }
                 }
+            } else if(exp[n] == "\\") {
+                newprops.escaped = true;
             }
             n++;
         }
