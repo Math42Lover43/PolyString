8e72b6934e5da301f6e29e3997cf9d9d970e093e
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2024-10-01 16:41:47 -0700

    Update polystring.js
    ?

--- polystring.js
+++ polystring.js
@@ -1,11 +1,11 @@
- // If "wild" is -1, unlimited length, >0: limited length
+ // If "wild" is -1, unlimited length, >0: limited length; bool = whether obedience is required or disobedience
 var PolyString = {
     "compile": function(exp) {
         var reqs = []; // format: {req,bool,mult,wild}
         var n = 0;
         var newprops = {"bool": true,"escaped":false};
         while(n < exp.length) {
-            if(escaped) {
+            if(newprops.escaped) {
                 if(reqs[reqs.length - 1] && reqs[reqs.length - 1].wild == 0) {
                     reqs[reqs.length - 1].req += exp[n];
                 } else {
@@ -28,6 +28,13 @@ var PolyString = {
                 }
             } else if(exp[n] == "\\") {
                 newprops.escaped = true;
+            } else if(exp[n] == "," && reqs[reqs.length - 1]) {
+                if(!reqs[reqs.length - 1].wild || reqs[reqs.length - 1].wild == 0) {
+                    if(typeof reqs[reqs.length - 1].req == "string"){
+                        reqs[reqs.length - 1].wild = undefined;
+                        reqs[reqs.length - 1].req = [reqs[reqs.length - 1].req,""];
+                    }
+                }
             }
             n++;
         }
