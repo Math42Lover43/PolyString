8e37916fd4656fe7b0f21366656fdec6226677fd
Sai Deepak <160340448+Math42Lover43@users.noreply.github.com>
2024-11-27 20:59:25 -0800

    Update polystring.js
    Reform?

--- polystring.js
+++ polystring.js
@@ -1,14 +1,23 @@
 var PolyString = {
     "compile": function(exp) {
-        var reqs = []; // format: characters: {[index, identifiers*], allowed characters, bool}; multitudes: {ands, multitude strings, identifier}
+        var reqs = []; // format: characters: {allowed characters, bool}; multitudes: {ands, multitude strings, identifier}
         var array_stack = [0];
         var n = 0;
+        var i = 0;
         var newprops = {"bool": true,"escaped":false};
         var add = function(arg) {
-            reqs[array_stack[0]].characters.allowedcharacters += arg;
+            reqs[array_stack[0]].characters += arg;
             newprops.escaped = false;
         };
         while(n < exp.length) {
+            if(!reqs[array_stack[0]) {
+                reqs[array_stack[0] = [
+                    {
+                        "characters":"",
+                        "equiv":true
+                    }
+                ];
+            }
             if(newprops.escaped) {
                 add(exp[n]);
             }
